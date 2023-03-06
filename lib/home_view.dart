1344cf378ad627115d27d3e838ccb24b3d255e8e
Dogan Gunes <dogangns.98@gmail.com>
2023-03-07 01:34:01 +0300

    .

---
+++ lib/home_view.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class Homeview extends StatefulWidget {
+  const Homeview({super.key});
+
+  @override
+  State<Homeview> createState() => _HomeviewState();
+}
+
+class _HomeviewState extends State<Homeview> {
+  Location location = new Location();
+
+  late bool _serviceEnabled;
+  PermissionStatus? _permissionGranted;
+  LocationData? _locationData;
+
+  void getLocation() async {
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+    _locationData = await location.getLocation();
+    location.onLocationChanged.listen((LocationData event) {
+       print("Lat ${event.latitude} ve Lon ${event.longitude} ");
+    });
+  }
+
+@override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Socket App")),
+    );
+  }
+}
